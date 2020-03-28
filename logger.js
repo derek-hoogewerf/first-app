cbf638b0057bc3f4347ab016da988cc3b27aa33d
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-03-28 20:59:37 +0530

    updated files

--- logger.js
+++ logger.js
@@ -1,6 +1,3 @@
-
-    console.log(__filename);
-    console.log(__dirname);
 var url = 'http://mylogger.io/log';
 
 function log(message)
@@ -8,8 +5,5 @@ function log(message)
     //send an HTTP request
     console.log(message);
 }
-
- module.exports = log;
- //module.exports.endPoint = url;
-
- 
+ module.exports.log = log;
+ //module.exports.endPoint = url; 
